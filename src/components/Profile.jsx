805ce4aefc48bf386cff5851cafe011ae5323ed9
1l0v3m0un741n5 <>
2021-09-24 11:53:38 +0300

    add: logout functionality for profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,20 +2,35 @@ import withUser from '../hoc/withUser';
 import { useEffect, useState } from 'react'
 import { LoginAPI } from '../api/LoginAPI';
 import { useId } from '../context/IdContext'
+import { useUser } from '../context/UserContext'
 import ProfileItem from './ProfileItem';
+import { useHistory } from "react-router-dom";
 
 function Profile() {
 	const [ items, setItems ] = useState([])
-	const { id } = useId()
+	const { id, setId } = useId()
+	const { user, setUser } = useUser()
+
+	let history = useHistory()
+
+	const handleLogoutClick = () => {
+		setUser('')
+		setId('')
+	}
 
 	useEffect(() => {
-		LoginAPI.getTranslations(id).then(data => {
-			setItems(data.translations)
-		})
-		.catch(error => {
-			console.log(error)
-		})
-	}, [])
+		if(user === '') {
+			history.push('/')
+		} else {
+			LoginAPI.getTranslations(id).then(data => {
+				setItems(data.translations)
+			})
+			.catch(error => {
+				console.log(error)
+			})
+		}
+
+	}, [user])
 	
 	return (
 		<div className="profile">
@@ -33,6 +48,7 @@ function Profile() {
 					}
 				</tbody>
 			</table>
+			<button onClick= { handleLogoutClick }>Logout</button>
 		</div>
 	)
 }
